5681af6b020aa988b3f38261eb602f71ca05c2ca
gunrein <gunrein620@gmail.com>
2026-04-07 01:36:51 +0900

    BT-4: sumOfNodes

--- Data-Structures/Binary_Tree/Q4_E_BT.c
+++ Data-Structures/Binary_Tree/Q4_E_BT.c
@@ -104,6 +104,15 @@ int sumOfOddNodes(BTNode *node)
 
 {
     /* add your code here */
+    if(node == NULL) return 0; // base case
+
+    int oddSum = 0;
+
+    if(node->item % 2!=0)
+        oddSum = node->item;
+    return current
+        + sumOfOddNodes(node->left)
+        + sumOfOddNodes(node->right);
 }
 
 //////////////////////////////////////////////////////////////////////////////////
